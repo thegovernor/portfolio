93eaa8b33810e9b20ae04e2048f6e6b471dfd49a
zakaria <zakaria@192.168.1.3>
2022-07-03 11:40:23 +0100

    changes

--- src/Components/Main-banner/Header.js
+++ src/Components/Main-banner/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Wrapper,Header,Div1,Div2,Div3, Logo, Circle,Section,H1,Paragraph, Button,} from "./Header.styled";
 import Ben from "../Images/sirzakariaben5.jpeg"
-import {FaInstagramSquare,FaMedium,FaGithubSquare} from "react-icons/fa"
+import {FaInstagramSquare,FaMedium,FaGithubSquare,FaLinkedin} from "react-icons/fa"
 import { Link} from "react-scroll";
 
 export default function Container(){
@@ -16,6 +16,7 @@ export default function Container(){
                 <a target="_blank" rel="noreferrer" href="https://www.instagram.com/sirzakariaben/"><FaInstagramSquare size="30px" color="#ffd166"/></a>
                 <a target="_blank" rel="noreferrer" href="https://medium.com/@zakariabenaddi"><FaMedium size="30px" color="#ffd166"/></a>
                 <a target="_blank" rel="noreferrer" href="https://github.com/thegovernor"><FaGithubSquare size="30px" color="#ffd166"/></a>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/zakaria-ben-addi-64395961/"><FaLinkedin size="30px" color="#ffd166"/></a>
             </Div3>
         </Header>
         <Section>
